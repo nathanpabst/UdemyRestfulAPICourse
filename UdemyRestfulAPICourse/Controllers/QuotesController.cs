0ad54ff9713029a319d8bda2e9615936431055a9
Nathan Pabst <Nathan.Pabst@ey.com>
2020-04-06 07:30:29 -0500

    adding query to sort quotes by asc or desc order.

--- UdemyRestfulAPICourse/Controllers/QuotesController.cs
+++ UdemyRestfulAPICourse/Controllers/QuotesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 using UdemyRestfulAPICourse.Data;
 using UdemyRestfulAPICourse.Models;
 
@@ -19,9 +20,22 @@ namespace UdemyRestfulAPICourse.Controllers
 
         // GET: api/Quotes
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string sort)
         {
-            return Ok(_quotesDbContext.Quotes);
+            IQueryable<Quote> quotes;
+            switch (sort)
+            {
+                case "desc":
+                    quotes = _quotesDbContext.Quotes.OrderByDescending(q => q.CreatedAt);
+                    break;
+                case "asc":
+                    quotes = _quotesDbContext.Quotes.OrderBy(q => q.CreatedAt);
+                    break;
+                default:
+                    quotes = _quotesDbContext.Quotes;
+                    break;
+            }
+            return Ok(quotes);
         }
 
         // GET: api/Quotes/5
